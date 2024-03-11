0a4e1748b274bdc831f2df1e747245f3a0116eaa
ShadowCore <aleksandr.bobyr911@gmail.com>
2024-03-11 21:57:59 +0200

    feat | update to v15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
-const routes: Routes = [{
-  path: '',
-  component: AppComponent,
-  children: [
-    {
-      path: ':userId',
-      loadComponent: () => import('./components/user-form/user-form.component').then(c => c.UserFormComponent)
-    }
-  ]
-}];
+const routes: Routes = [
+  {
+    path: ':userId',
+    loadComponent: () => import('./components/user-form/user-form.component').then(c => c.UserFormComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
