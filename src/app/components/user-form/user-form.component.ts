3582dab5ee5375079e3f6e13cf9f7fab61a8fc92
ShadowCore <aleksandr.bobyr911@gmail.com>
2024-03-12 00:19:22 +0200

    feat | add user form component styles

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UserTypeEnum } from '../../models/user-type.enum';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
-
-  constructor() { }
+  userTypeEnum = UserTypeEnum;
 
   ngOnInit(): void {
   }
