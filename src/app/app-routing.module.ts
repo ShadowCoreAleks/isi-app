31d819751931a01035994fbe9875240fa667a880
ShadowCore <aleksandr.bobyr911@gmail.com>
2024-03-11 21:53:52 +0200

    feat | create basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: AppComponent,
+  children: [
+    {
+      path: ':userId',
+      loadComponent: () => import('./components/user-form/user-form.component').then(c => c.UserFormComponent)
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/user-form/user-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss']
+})
+export class UserFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
