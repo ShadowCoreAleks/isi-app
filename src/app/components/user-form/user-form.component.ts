affeb99e8fbc2eaadea0a2c52377208a71cd8ed1
ShadowCore <aleksandr.bobyr911@gmail.com>
2024-03-13 00:57:58 +0200

    feat | added second variant

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import {
-  ChangeDetectionStrategy,
+  ChangeDetectionStrategy, ChangeDetectorRef,
   Component,
   computed,
   effect,
@@ -12,11 +12,11 @@ import {
 import { UserTypeEnum } from '../../models/user-type.enum';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { GenericButtonComponent } from '../generic-button/generic-button.component';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BackendApiService } from '../../services/backend-api.service';
-import { firstValueFrom } from 'rxjs';
+import { filter, firstValueFrom, map, Observable, switchMap, tap } from 'rxjs';
 import { IUser } from '../../models/user.interface';
-import { NgTemplateOutlet } from '@angular/common';
+import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
 
 enum PageModeEnum {
   create = 'create',
@@ -35,7 +35,8 @@ type UserType = IUser | null;
     GenericButtonComponent,
     RouterLink,
     ReactiveFormsModule,
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    AsyncPipe
   ],
   styleUrls: ['./user-form.component.scss']
 })
@@ -43,7 +44,10 @@ export class UserFormComponent implements OnInit {
   username = input.required<string>();
   $pageMode: Signal<PageModeEnum> = computed(() => !!this.username() ? PageModeEnum.update : PageModeEnum.create);
   $user: WritableSignal<UserType> = signal(null);
+  user$!: Observable<UserType>;
   #backendApiService = inject(BackendApiService);
+  #activatedRoute = inject(ActivatedRoute);
+  #cdr = inject(ChangeDetectorRef);
 
   protected readonly userTypeEnum = UserTypeEnum;
   protected readonly pageModeEnum = PageModeEnum;
@@ -60,9 +64,23 @@ export class UserFormComponent implements OnInit {
   constructor() {
     effect(async () => {
       if (this.username()) {
+
+        /** first variant */
         const user = await firstValueFrom(this.#backendApiService.loadSpecificUser(this.username())) || null;
         this.$user.set(user);
         this.fillForm(user);
+
+        /** second variant */
+        // this.user$ = this.#activatedRoute.paramMap.pipe(
+        //   map(params => params.get('username')),
+        //   filter(Boolean),
+        //   switchMap(username => this.#backendApiService.loadSpecificUser(username).pipe(
+        //     tap(user => {
+        //       this.fillForm(user);
+        //       this.#cdr.markForCheck();
+        //     })
+        //   ))
+        // )
       }
     });
   }
